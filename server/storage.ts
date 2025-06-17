8a481af128b3bc81028036cf41963d8eab175aa6
maverickcalamari <trey.d.brown982@gmail.com>
2025-06-17 15:57:07 -0400

    Update storage.ts

--- server/storage.ts
+++ server/storage.ts
@@ -9,6 +9,15 @@ import {
 } from "@shared/schema";
 import session from "express-session";
 import createMemoryStore from "memorystore";
+import bcrypt from "bcryptjs";
+
+export async function hashPassword(password: string) {
+  return await bcrypt.hash(password, 10);
+}
+
+export async function comparePasswords(supplied: string, stored: string) {
+  return await bcrypt.compare(supplied, stored);
+}
 
 const MemoryStore = createMemoryStore(session);
 
