905fafbc5129c6ef2e826ac0e8c9c075caaac312
maverickcalamari <trey.d.brown982@gmail.com>
2025-06-17 18:26:17 -0400

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
+import { hashPassword } from "./storage";
+import { hashPassword, comparePasswords } from "./storage";
 
 const app = express();
 app.use(express.json());
